72329538dacb4a31f6b32a0040703fdda18f28c5
Asi Baka <asibaka@Asis-MacBook-Pro.local>
2018-07-23 20:25:19 +0530

    Test plus people3

--- routes/index.js
+++ routes/index.js
@@ -22,10 +22,12 @@ router.get("/",function(req,res) {
 
 // ================== Sign up Route ======================
 router.get('/auth/google',
-  passport.authenticate('google', { scope: ['profile'] }));
+  passport.authenticate('google', { scope: [ 'https://www.googleapis.com/auth/plus.login',
+      'https://www.googleapis.com/auth/plus.profile.emails.read' ] }));
 
 router.get('/auth/google/callback', 
-  passport.authenticate('google', { scope:[ 'email', 'profile'],failureRedirect: '/login' }),
+  passport.authenticate('google', { scope:[ 'https://www.googleapis.com/auth/plus.login',
+      'https://www.googleapis.com/auth/plus.profile.emails.read' ],failureRedirect: '/login' }),
   function(req, res) {
     // Successful authentication, redirect home.
     res.redirect('/');
